5fbde3572f844fbc96e627510c963120e4b3fa84
luisandrespineda <luispineda@Luiss-Air.attlocal.net>
2023-10-04 23:33:24 -0500

    presentation code

--- test/static/js/test_js.js
+++ test/static/js/test_js.js
@@ -1,11 +1,15 @@
-let data;
+//Define gloabl variables that hold all of the data
+let percentage_data;
+let numbers_data;
+let injuries_data;
 
 d3.json('/api/collision').then(function(responseData) {
-    
     let brgh_names = responseData.map(function (row){
         return row.borough;
     });
     
+    console.log(brgh_names)
+
     // Trace for the Greek Data
     let trace1 = {
         x: responseData.map(row => row.borough),
@@ -18,14 +22,99 @@ d3.json('/api/collision').then(function(responseData) {
     
     // Apply a title to the layout
     let layout = {
-        title: "Crashes by borough"
+        title: "Crashes by Borough"
     };
     
     // Render the plot to the div tag with id "bar"
-    Plotly.newPlot("bar", plotData, layout);
+    Plotly.newPlot("bar1", plotData, layout);
     
     console.log(responseData);
+
+    const dropdown = document.getElementById("selDataset");
+    for (const option of brgh_names) {
+      const optionElement = document.createElement("option");
+      optionElement.value = option; // Set the value
+      optionElement.textContent = option; // Set the displayed text
+      dropdown.appendChild(optionElement); // Add the option to the dropdown
+    }
 });
 
 
+d3.json('/api/collision_deaths').then(function(collision_data) {
+    percentage_data = collision_data;
+    console.log(percentage_data);
+});
 
+d3.json('/api/collision_deaths_num').then(function(nums_data) {
+    numbers_data = nums_data;
+    console.log(numbers_data);
+});
+
+d3.json('/api/injuries').then(function(inj_data) {
+    injuries_data = inj_data;
+    console.log(injuries_data);
+});
+
+
+function optionChanged() {
+    let dropdownMenu = d3.select("#selDataset");
+    let datasetId = dropdownMenu.property("value");
+    console.log('datasetId',datasetId)
+    console.log('test data pull',numbers_data)
+
+    let selectedBoroughData = numbers_data.find(entry => entry["borough"] === datasetId.toString());
+    let selectedBoroughpie = percentage_data.find(entry => entry["borough"] === datasetId.toString());
+    let selectedBoroughinjbar = injuries_data.find(entry => entry["borough"] === datasetId.toString());
+
+    console.log('selected_borough_data',selectedBoroughData)
+
+    if (selectedBoroughData) {
+        console.log('test meta data', selectedBoroughData);
+        console.log('test ID', datasetId);
+
+        let panelBody = d3.select(".panel-body");
+        panelBody.html("");
+
+        let metadataItems = [
+            `Cyclist Deaths: ${selectedBoroughData.cyclist_death}`,
+            `Motorist Deaths: ${selectedBoroughData.motorist_death}`,
+            `Pedestrian Deaths: ${selectedBoroughData.pedestrian_death}`,
+        ];
+
+        metadataItems.forEach(item => {
+            panelBody.append("p").text(item);
+        });
+    } else {
+        console.log(`No data found for borough with ID: ${datasetId}`);
+    }
+
+    var pie_data = [{
+        values: [selectedBoroughpie.cyclist_death_percentage, selectedBoroughpie.motorist_death_percentage
+            , selectedBoroughpie.pedestrian_death_percentage, selectedBoroughpie.person_death_percentage],
+        labels: ['Cyclist', 'Motorist', 'Pedestrian','Person'],
+        type: 'pie'
+    }];
+      
+    var layout = {
+        title: 'Percentage of Deaths by Type',
+        height: 400,
+        width: 500
+    };
+    
+      Plotly.newPlot('pie_chart', pie_data, layout);
+    
+      var inj_bar_data = [
+        {
+          x: ['Cyclists Injured','Motorists Injured','Pedestrians Injured','Total Injured'],
+          y: [selectedBoroughinjbar.cyclists_injured,selectedBoroughinjbar.motorists_injured,
+            selectedBoroughinjbar.pedestrians_injured,selectedBoroughinjbar.persons_injured],
+          type: 'bar'
+        }
+      ];
+  
+      var layout2 = {
+        title: 'Injuries in the Selected Burough',
+    };
+
+      Plotly.newPlot('bar2', inj_bar_data,layout2 );
+    }
--- test/test_app.py
+++ test/test_app.py
@@ -16,6 +16,9 @@ Base.prepare(autoload_with=engine)
 
 # Map the appropriate class to your PostgreSQL table
 borough = Base.classes.brgh_collisions
+percentages = Base.classes.death_per_table
+numbers = Base.classes.num_death_per_table
+injuries = Base.classes.total_injuries
 
 @app.route('/')
 def welcome():
@@ -26,7 +29,7 @@ def welcome():
 # Define a route to serve the dashboard page
 
 @app.route('/api/collision')
-def get_data():
+def borough_collision_data():
     # Create a session to interact with the database
     session = Session(engine)
 
@@ -42,6 +45,60 @@ def get_data():
         borough_collisions_results.append(row_dict)
 
     return jsonify(borough_collisions_results)
+
+@app.route('/api/collision_deaths')
+def percentage_data():
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # Query the database to retrieve the data you want
+    results = session.query(percentages).all()
+
+    session.close()
+
+    # Convert query results to a list of dictionaries
+    percentages_results = []
+    for row in results:
+        row_dict = {column.name: getattr(row, column.name) for column in percentages.__table__.columns}
+        percentages_results.append(row_dict)
+
+    return jsonify(percentages_results)
+
+@app.route('/api/collision_deaths_num')
+def num_data():
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # Query the database to retrieve the data you want
+    results = session.query(numbers).all()
+
+    session.close()
+
+    # Convert query results to a list of dictionaries
+    numbers_results = []
+    for row in results:
+        row_dict = {column.name: getattr(row, column.name) for column in numbers.__table__.columns}
+        numbers_results.append(row_dict)
+
+    return jsonify(numbers_results)
+
+@app.route('/api/injuries')
+def injuries_data():
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # Query the database to retrieve the data you want
+    results = session.query(injuries).all()
+
+    session.close()
+
+    # Convert query results to a list of dictionaries
+    injuries_results = []
+    for row in results:
+        row_dict = {column.name: getattr(row, column.name) for column in injuries.__table__.columns}
+        injuries_results.append(row_dict)
+
+    return jsonify(injuries_results)
 
 if __name__ == '__main__':
     app.run(debug=True)
